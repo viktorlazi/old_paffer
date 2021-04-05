86263366ceea61fc1a86cd96fd74904bbac7aa0d
Viktorlazi <viktorlazi010@gmail.com>
2021-04-05 15:32:01 +0200

    loading

--- contracts/Paffer.sol
+++ contracts/Paffer.sol
@@ -2,8 +2,8 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Paffer{
   string public name = "Paffer";
-  
   uint public paffCount = 0;
+  
   struct Paff{
     uint id;
     string hash;
--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 import Home from './Pages/Home/Home'
 import Header from './Components/Header'
 import Paffer from './abis/Paffer.json'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import Web3 from 'web3'
 
 function App() {  
+  const [paffer, setPaffer] = useState(null)
+  const [loading, setLoading]= useState(true)
+  const [account, setAccount]= useState()
+
   const loadWeb3 = async () =>{
     if(window.ethereum){
       window.web3 = new Web3(window.ethereum)
@@ -19,24 +23,32 @@ function App() {
   const loadBlockchainData = async () =>{
     const web3 = window.web3
     const accounts = await web3.eth.getAccounts()
-    
+    setAccount(accounts[0])
+
     const networkId = await web3.eth.net.getId()
     const networkData = Paffer.networks[networkId]
     if(networkData){
-      const paffer = web3.eth.Contract(Paffer.abi, networkData.address)
-      console.log(paffer)
-      
+      const paffer = new web3.eth.Contract(Paffer.abi, networkData.address)
+      setPaffer(paffer)
+      setLoading(false)
     }else{
       window.alert('paffer not deployed to network')
     }
   }
-
-  useEffect( async () => {
+  
+  const load = async () =>{
     await loadWeb3()
     await loadBlockchainData()
+  }
+
+
+  useEffect(() => {
+    load()
   }, [])
   
   return (
+    loading ? 
+    <p>loading</p> :
     <div className="App">
       <Header />
       <Home />
